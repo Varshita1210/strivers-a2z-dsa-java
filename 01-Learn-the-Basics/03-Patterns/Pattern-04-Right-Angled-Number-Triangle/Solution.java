a995f0acb2a4c3a359ff8d90cc1751fdcf12fa35
Varshita Pallapothu <varshita101206@gmail.com>
2026-07-29 23:18:09 +0530

    Solved Patterns 1-6

---
+++ 01-Learn-the-Basics/03-Patterns/Pattern-04-Right-Angled-Number-Triangle/Solution.java
@@ -0,0 +1,25 @@
+/*
+Pattern:
+
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+*/
+
+class Solution {
+
+    public void pattern4(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
